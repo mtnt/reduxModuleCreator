413862c5c07ba280de70a923431299b27aca45a1
Kirill Glazunov <reg@webmutant.ru>
2018-04-03 12:44:09 +0300

    Reorganize tests for module

---
+++ tests/module.dispatch.test.js
@@ -0,0 +1,162 @@
+import {noop} from 'lodash';
+
+import {unlinkStore, RMCCtl} from "../src";
+import {getActionCreator, creator} from './helpers';
+
+const payload0 = {
+  name: "payload0",
+  value: true,
+};
+const payload1 = {
+  name: "payload1",
+  value: false,
+};
+const initialData = {
+  name: "initial",
+};
+
+afterEach(() => {
+  unlinkStore();
+});
+
+describe('module.dispatch', () => {
+  it("does not affect deinitialized module", () => {
+    const stateDidUpdate = jest.fn();
+    const actionCreator = getActionCreator();
+    function reducer(state = initialData, action) {
+      switch(action.type) {
+        case actionCreator.type:
+          return action.payload;
+
+        default:
+          return state;
+      }
+    }
+    class Ctl extends RMCCtl {
+      _stateDidUpdate(...args) {
+        stateDidUpdate(...args);
+      }
+    }
+    const module = creator(reducer, Ctl);
+
+    module.deinitialize();
+    module.dispatch(actionCreator(payload0));
+
+    expect(stateDidUpdate).toHaveBeenCalledTimes(0);
+  });
+
+  it('called single time', () => {
+    const stateDidUpdate = jest.fn();
+    const actionCreator = getActionCreator();
+    function reducer(state = initialData, action) {
+      switch(action.type) {
+        case actionCreator.type:
+          return action.payload;
+
+        default:
+          return state;
+      }
+    }
+    class Ctl extends RMCCtl {
+      _stateDidUpdate(...args) {
+        stateDidUpdate(...args);
+      }
+    }
+    const module = creator(reducer, Ctl);
+
+    module.dispatch(actionCreator(payload0));
+
+    // should call `_stateDidUpdate`
+    expect(stateDidUpdate).toHaveBeenCalledTimes(1);
+
+    // should call `_stateDidUpdate` with previousState
+    expect(stateDidUpdate).toHaveBeenCalledWith(initialData);
+
+    // should change own state property to new state
+    expect(module.ownState).toEqual(payload0);
+  });
+
+  it('called several times with a same action but different payloads', () => {
+    const stateDidUpdate = jest.fn();
+    const actionCreator = getActionCreator();
+    function reducer(state = initialData, action) {
+      switch(action.type) {
+        case actionCreator.type:
+          return action.payload;
+
+        default:
+          return state;
+      }
+    }
+    class Ctl extends RMCCtl {
+      _stateDidUpdate(...args) {
+        stateDidUpdate(...args);
+      }
+    }
+    const module = creator(reducer, Ctl);
+
+    module.dispatch(actionCreator(payload0));
+    module.dispatch(actionCreator(payload1));
+
+    // should call `stateDidUpdate` each time
+    expect(stateDidUpdate).toHaveBeenCalledTimes(2);
+
+    // should call `stateDidUpdate` with a result of handling a last action
+    expect(stateDidUpdate).toHaveBeenLastCalledWith(payload0); // previous state
+
+    // should change own state property a result of handling a last action
+    expect(module.ownState).toEqual(payload1);
+  });
+
+  it('called several times with totally same actions including payloads', () => {
+    const stateDidUpdate = jest.fn();
+    const actionCreator = getActionCreator();
+    function reducer(state = initialData, action) {
+      switch(action.type) {
+        case actionCreator.type:
+          return action.payload;
+
+        default:
+          return state;
+      }
+    }
+    class Ctl extends RMCCtl {
+      _stateDidUpdate(...args) {
+        stateDidUpdate(...args);
+      }
+    }
+    const module = creator(reducer, Ctl);
+
+    module.dispatch(actionCreator(payload0));
+    module.dispatch(actionCreator(payload0));
+
+    // should call `stateDidUpdate` just single time
+    expect(stateDidUpdate).toHaveBeenCalledTimes(1);
+  });
+
+  it('called with an action that shouldn`t affect module`s state', () => {
+    const stateDidUpdate = jest.fn();
+    const actionCreator0 = getActionCreator();
+    const actionCreator1 = getActionCreator();
+    function reducer(state = initialData, action) {
+      switch(action.type) {
+        case actionCreator0.type:
+          return action.payload;
+
+        default:
+          return state;
+      }
+    }
+    class Ctl extends RMCCtl {
+      _stateDidUpdate(...args) {
+        stateDidUpdate(...args);
+      }
+    }
+    const module = creator(reducer, Ctl);
+
+    module.dispatch(actionCreator1(payload1));
+
+    // should not call `stateDidUpdate` at all
+    expect(stateDidUpdate).toHaveBeenCalledTimes(0);
+  });
+});
---
+++ tests/module.ownState.test.js
@@ -0,0 +1,193 @@
+import {cloneDeep} from 'lodash';
+
+import {unlinkStore, RMCCtl} from "../src";
+import {getActionCreator, creator} from './helpers';
+
+const VALID_CLASS = class SCtl extends RMCCtl {
+  _stateDidUpdate() {}
+};
+const MODULE_REDUCER = () => {
+  return {
+    name: "initial",
+  };
+};
+
+afterEach(() => {
+  unlinkStore();
+});
+
+describe("module.ownState", () => {
+  describe("for getting data", () => {
+    it("should be accessible from outside", () => {
+      const ownState = {
+        foo: 'bar',
+        bar: 'foo',
+      };
+      const expected = cloneDeep(ownState);
+      const reducer = (state = {}, action) => {
+        return ownState;
+      };
+      const module = creator(reducer, VALID_CLASS);
+
+      expect(module.ownState).toEqual(expected);
+    });
+
+    it("should be accessible from inside of controller`s method", () => {
+      const ownState = {
+        foo: 'bar',
+        bar: 'foo',
+      };
+      const expected = cloneDeep(ownState);
+      const reducer = (state = {}, action) => {
+        return ownState;
+      };
+      class Ctl extends VALID_CLASS {
+        getOwnState() {
+          return this.ownState;
+        }
+      }
+      const module = creator(reducer, Ctl);
+
+      expect(module.getOwnState()).toEqual(expected);
+    });
+
+    it("should be accessible from inside of the `_stateDidUpdate` method", () => {
+      const actionCreator = getActionCreator();
+      const someFunc = jest.fn();
+      const ownState = {
+        foo: 'bar',
+        bar: 'foo',
+      };
+      const expected = cloneDeep(ownState);
+      function reducer(state = {}, action) {
+        switch(action.type) {
+          case actionCreator.type:
+            return action.payload;
+
+          default:
+            return state;
+        }
+      }
+      class Ctl extends VALID_CLASS {
+        _stateDidUpdate() {
+          someFunc(this.ownState);
+        }
+      }
+      const module = creator(reducer, Ctl);
+
+      module.dispatch(actionCreator(ownState));
+
+      expect(someFunc).toHaveBeenCalledWith(expected);
+    });
+  });
+
+  describe("for setting whole state", () => {
+    it("shouldn`t be accessible from outside", () => {
+      const module = creator(MODULE_REDUCER, VALID_CLASS);
+
+      expect(() => {
+        module.ownState = {};
+      }).toThrow();
+    });
+
+    it("shouldn`t be accessible from inside of controller`s method", () => {
+      class Ctl extends VALID_CLASS {
+        someMethod() {
+          this.ownState = {};
+        }
+      }
+      const module = creator(MODULE_REDUCER, Ctl);
+
+      expect(() => {
+        module.someMethod();
+      }).toThrow();
+    });
+
+    it("shouldn`t be accessible from inside of the `_stateDidUpdate` method", () => {
+      const actionCreator = getActionCreator();
+      function reducer(state = {}, action) {
+        switch(action.type) {
+          case actionCreator.type:
+            return action.payload;
+
+          default:
+            return state;
+        }
+      }
+      class Ctl extends VALID_CLASS {
+        _stateDidUpdate() {
+          this.ownState = {};
+        }
+      }
+      const module = creator(reducer, Ctl);
+
+      expect(() => {
+        module.dispatch(actionCreator());
+      }).toThrow();
+    });
+  });
+
+  describe("for setting a part of the state", () => {
+    it("shouldn`t be accessible from outside", () => {
+      const ownState = {
+        foo: {
+          bar: 'foo',
+        },
+      };
+      const expected = cloneDeep(ownState);
+      function reducer(state = {}, action) {
+        return ownState;
+      }
+      const module = creator(reducer, VALID_CLASS);
+
+      module.ownState.foo = {};
+
+      expect(module.ownState).toEqual(expected);
+    });
+
+    it("shouldn`t be accessible from inside of controller`s method", () => {
+      const ownState = {
+        foo: {
+          bar: 'foo',
+        },
+      };
+      const expected = cloneDeep(ownState);
+      function reducer(state = {}, action) {
+        return ownState;
+      }
+      class Ctl extends VALID_CLASS {
+        someMethod() {
+          this.ownState.foo = {};
+        }
+      }
+      const module = creator(reducer, Ctl);
+
+      module.someMethod();
+
+      expect(module.ownState).toEqual(expected);
+    });
+
+    it("shouldn`t be accessible from inside of the `_stateDidUpdate` method", () => {
+      const actionCreator = getActionCreator();
+      const ownState = {
+        foo: {
+          bar: 'foo',
+        },
+      };
+      const expected = cloneDeep(ownState);
+      function reducer(state = {}, action) {
+        return ownState;
+      }
+      class Ctl extends VALID_CLASS {
+        _stateDidUpdate() {
+          this.ownState.foo = {};
+        }
+      }
+      const module = creator(reducer, Ctl);
+
+      module.dispatch(actionCreator());
+
+      expect(module.ownState).toEqual(expected);
+    });
+  });
+});
