709b765326a7cfe9224457f2acc9e33ce2ab4e75
Kirill Glazunov <reg@webmutant.ru>
2022-03-29 16:07:22 +0300

    Extend errors tests

--- tests/baseErrors.test.js
+++ tests/baseErrors.test.js
@@ -1,20 +1,33 @@
-import {DuplicateError, RMCError} from '../src/lib/baseErrors';
+import {
+  DuplicateError,
+  InsufficientDataError,
+  InvalidParamsError,
+  WrongInterfaceError,
+  RMCError,
+} from '../src/lib/baseErrors';
 
-describe('errors', () => {
+describe.each([
+  ['DuplicateError', new DuplicateError('message')],
+  ['InsufficientDataError', new InsufficientDataError('message')],
+  ['InvalidParamsError', new InvalidParamsError('message')],
+  ['WrongInterfaceError', new WrongInterfaceError('message')],
+])('errors, %s', (name, error) => {
   afterEach(() => {});
 
-  it('should be instance of Error', () => {
-    expect(new DuplicateError('message')).toBeInstanceOf(Error);
+  it(`${name} should have a name prop`, () => {
+    expect(error.name).not.toBeNull();
   });
 
-  it('should be instance of RMCError', () => {
-    expect(new DuplicateError('message')).toBeInstanceOf(RMCError);
+  it(`${name} should be instance of Error`, () => {
+    expect(error).toBeInstanceOf(Error);
   });
 
-  it('message should contain all parents classNames', () => {
-    const error = new DuplicateError('message');
+  it(`${name} should be instance of RMCError`, () => {
+    expect(error).toBeInstanceOf(RMCError);
+  });
 
+  it(`${name}\`s message should contain all parents classNames`, () => {
     expect(error.message).toContain(RMCError.name);
-    expect(error.message).toContain(DuplicateError.name);
+    expect(error.message).toContain(error.name);
   });
 });
