5ee8a45403af255fed5652c667f54a6e25d5acc3
Kirill Glazunov <reg@webmutant.ru>
2022-03-29 16:47:59 +0300

    Add a default state value for the combineReducers

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -37,7 +37,7 @@ export function normalizePath(path) {
 }
 
 export function combineReducers(stateReducerMap, rootPath) {
-  return function(state, action) {
+  return function(state = {}, action) {
     let changed = false;
 
     const nextState = {};
